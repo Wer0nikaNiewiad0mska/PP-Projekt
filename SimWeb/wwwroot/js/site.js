37beba1b7a6f68e4f218427c384f82e6f79c5ef3
Aleksandra Marasek <232418@student.uek.krakow.pl>
2025-01-17 22:30:07 +0100

    coś

--- SimWeb/wwwroot/js/site.js
+++ SimWeb/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase(); // Pobierz klawisz w formie małych liter
+    let buttonId;
+    switch (key) {
+        case 'w':
+            buttonId = 'move-up';
+            break;
+        case 'a':
+            buttonId = 'move-left';
+            break;
+        case 's':
+            buttonId = 'move-down';
+            break;
+        case 'd':
+            buttonId = 'move-right';
+            break;
+        case 'e':
+            buttonId = 'collect-key';
+            break;
+        case 'q':
+            buttonId = 'open-lock';
+            break;
+        case 'i':
+            buttonId = 'open-inventory';
+            break;
+        case 'u':
+            buttonId = 'collect-potion';
+            break;
+        case 'r':
+            buttonId = 'activate-follower';
+            break;
+        default:
+            return; // Ignoruj inne klawisze
+    }
+    // Znajdź odpowiedni przycisk i wyślij zdarzenie kliknięcia
+    const button = document.getElementById(buttonId);
+    if (button) {
+        button.click();
+    }
+});
--- Simulation/MapInitialization.cs
+++ Simulation/MapInitialization.cs
@@ -17,13 +17,19 @@ public static class GameInitialization
         var player = new Player("Hero");
 
         // Ustawienia mapy BigMap
-        bigMap.AddBlockedField(new Point(1, 1));
+        bigMap.AddBlockedField(new Point(7, 8));
+        bigMap.AddBlockedField(new Point(8, 8));
+        bigMap.AddBlockedField(new Point(9, 8));
+
+        bigMap.AddUnlockedField(new Point(7,9),1,"");
+        bigMap.AddKey(new Point(2, 2), 1);
+
         bigMap.AddPotion(new Point(5, 5), "DoubleMovement");
         // Dodaj followera
         var triggerPoint = new Point(8, 9); // Punkt, którego odblokowanie aktywuje followera
         var follower = new Follower("Wercia", triggerPoint);
         bigMap.Add(follower, new Point(9, 9)); // Początkowa pozycja followera
-        follower.InitMapAndPosition(bigMap, new Point(9, 1));
+        follower.InitMapAndPosition(bigMap, new Point(9, 9));
 
         // Dodaj triggerujące pole na mapie
         bigMap.AddTriggerPoint(triggerPoint);
@@ -33,16 +39,30 @@ public static class GameInitialization
         var Olamo = new Npc("Olamo", "Hejeczka", 'O');
         Olamo.InitMapAndPosition(bigMap, new Point(4,4));
 
+        //DRUGA MAPA
+        secondMap.AddBlockedField(new Point(0,2));
+        secondMap.AddBlockedField(new Point(1, 2));
+        secondMap.AddBlockedField(new Point(2, 2));
+        secondMap.AddBlockedField(new Point(3, 2));
+        secondMap.AddBlockedField(new Point(4, 2));
+        secondMap.AddBlockedField(new Point(5, 2));
+        secondMap.AddBlockedField(new Point(6, 2)); 
+        secondMap.AddBlockedField(new Point(7, 2));
+
+
+        // Dodaj pole teleportacyjne na BigMap
+        var teleportToSecond = new TeleportField(new Point(9, 0), "SecondMap", new Point(0, 0));
+        teleportToSecond.InitMapAndPosition(bigMap, new Point(9, 0));
+
+
         var npc2 = new Npc("RandomGuy", "Miau miau", 'R');
         npc2.InitMapAndPosition(secondMap, new Point(9,9));
 
-        // Dodaj pole teleportacyjne na BigMap
-        var teleportToSecond = new TeleportField(new Point(0, 9), "SecondMap", new Point(0, 0));
-        teleportToSecond.InitMapAndPosition(bigMap, new Point(9, 9));
+        
 
         // Dodaj pole teleportacyjne na SecondMap
-        var teleportToBig = new TeleportField(new Point(4, 4), "BigMap", new Point(2, 2));
-        teleportToBig.InitMapAndPosition(secondMap, new Point(4, 4));
+        var teleportToBig = new TeleportField(new Point(0, 1), "BigMap", new Point(9, 1));
+        teleportToBig.InitMapAndPosition(secondMap, new Point(0, 1));
 
         // Dodaj gracza na BigMap
         player.InitMapAndPosition(bigMap, new Point(0, 0));
